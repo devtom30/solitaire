bd163a0018f62500f25916076d1a9f5d33c6ab6f
devtom30 <devtom30@gmail.com>
2015-12-04 13:20:00 +0100

    a few more tests

--- packages/solitaire.findallsolutions/solitaire.findallsolutions-tests.js
+++ packages/solitaire.findallsolutions/solitaire.findallsolutions-tests.js
@@ -2,6 +2,20 @@
  * Created by tom on 24/11/15.
  */
 
+var x = SOLCORE.Case.TypeEnum.FORBIDDEN;
+var o = SOLCORE.Case.TypeEnum.FULL;
+var e = SOLCORE.Case.TypeEnum.EMPTY;
+
+var initialGrid = [
+    [x, x, o, o, o, x, x],
+    [x, x, o, o, o, x, x],
+    [o, o, o, o, o, o, o],
+    [o, o, o, e, o, o, o],
+    [o, o, o, o, o, o, o],
+    [x, x, o, o, o, x, x],
+    [x, x, o, o, o, x, x],
+];
+
 Tinytest.add('GameLauncher constructor', function (test) {
     var gameLauncher = new GameLauncher();
     test.isNotNull(gameLauncher, 'gameLauncher is null');
@@ -26,8 +40,18 @@ Tinytest.add('gamelauncher findPossibleGrids', function (test) {
     var gameLauncher = new GameLauncher();
     gameLauncher.createGame();
     var possibleMoves = gameLauncher.getPlateau().findAllPossibleMoves();
-    test.isNotNull(possibleMoves);
 
     var possGrids = gameLauncher.getPlateau().findAllPossibleNextGrids();
+    test.isNotNull(possGrids);
+    // test if this is an array
+    test.isTrue(possGrids.constructor === Array);
+});
 
+Tinytest.add('gamelauncher findPossibleGrids in depth', function (test) {
+    var gameLauncher = new GameLauncher();
+    gameLauncher.createGame();
+    var possibleMoves = gameLauncher.getPlateau().findAllPossibleMoves();
+
+    var possGrids = gameLauncher.getPlateau().findAllPossibleNextGrids();
+    // now we test what is in this array
 });
