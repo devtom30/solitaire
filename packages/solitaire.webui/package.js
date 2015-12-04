4095b0ba1ba9a24659e173d6a3e55f53fe4b9bf5
devtom30 <devtom30@gmail.com>
2015-12-04 14:27:10 +0100

    indentation...

--- packages/solitaire.webui/package.js
+++ packages/solitaire.webui/package.js
@@ -6,23 +6,24 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
-  api.use('templating');
-  api.use('solitaire.core');
-  api.addFiles('namespaces.js');
-  api.export('SOLGAMEUI', ['client', 'server']);
-  api.addFiles(
-    [
-      'client/board.html', 
-      'client/grid.html', 
-      'client/cuteGrid.html', 
-      'client/cuteSquare.html',
-      'client/board.js', 
-      'client/grid.js',
-      'client/cuteGrid.js', 
-      'client/cuteSquare.js',
-      'lib/SolitaireGameUI.js'
-      ], 
-      'client');
-  
+    api.versionsFrom('1.0.2.1');
+    api.use('templating');
+    api.use('solitaire.core');
+    api.addFiles('namespaces.js');
+    api.export('SOLGAMEUI', ['client', 'server']);
+    api.addFiles(
+        [
+            'client/board.html',
+            'client/grid.html',
+            'client/cuteGrid.html',
+            'client/cuteSquare.html',
+            'client/board.js',
+            'client/grid.js',
+            'client/cuteGrid.js',
+            'client/cuteSquare.js',
+            'lib/SolitaireGameUI.js'
+        ],
+        'client'
+    );
+
 });
